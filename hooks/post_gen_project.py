23b08d24405badeb88461006d29426ab452a2ac4
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:29:08 +0100

    Add inital commit to post generate hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,5 +4,7 @@ import subprocess
 src = os.path.join(os.getcwd(), 'src', 'utils', 'prepare-commit-msg.py')
 dst = os.path.join('.git', 'hooks', 'prepare-commit-msg')
 
-process = subprocess.call(['git', 'init'])
+subprocess.call(['git', 'init'])
 os.symlink(src, dst)
+subprocess.call(['git', 'add', '-A'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
