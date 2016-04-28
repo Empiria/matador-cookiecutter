f77458e72407b1108ae8fe01dba3eb9f49464cf7
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:16:01 +0100

    Use os.path.join in post generate hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,9 @@
 import os
 import subprocess
 
-src = '../../src/utils/prepare-commit-msg.py'
-dst = '.git/hooks/prepare-commit-msg'
+
+src = os.path.join(os.getcwd(),'src', 'utils', 'prepare-commit-msg.py')
+dst = os.path.join('.git', 'hooks', 'prepare-commit-msg')
 
 process = subprocess.call(['git', 'init'])
 os.symlink(src, dst)
