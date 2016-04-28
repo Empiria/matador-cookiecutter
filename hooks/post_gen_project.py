2febb2e53a7f0b1a0a6952e4ea31c077f45b89f8
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:04:28 +0100

    Remove creation of hooks directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,12 +2,8 @@ import os
 import subprocess
 
 project_dir = '{{cookiecutter.repo_name}}'
-hooks_dir = os.path.join(project_dir, '.git/hooks')
-
 src = os.path.join(project_dir, 'src/utils/prepare-commit-msg.py')
-dst = os.path.join(hooks_dir, 'prepare-commit-msg')
+dst = os.path.join(project_dir, '.git/hooks/prepare-commit-msg')
 
 process = subprocess.call(['git', 'init', project_dir])
-
-os.mkdir('{{cookiecutter.repo_name}}/.git/hooks')
 os.symlink(src, dst)
