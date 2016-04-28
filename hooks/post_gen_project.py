93aa07471f4a4a04676c66b52f04d3ee529e951e
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:02:41 +0100

    Correct argument to subprocess.call

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,7 +7,7 @@ hooks_dir = os.path.join(project_dir, '.git/hooks')
 src = os.path.join(project_dir, 'src/utils/prepare-commit-msg.py')
 dst = os.path.join(hooks_dir, 'prepare-commit-msg')
 
-process = subprocess.call('git', 'init', project_dir)
+process = subprocess.call(['git', 'init', project_dir])
 
 os.mkdir('{{cookiecutter.repo_name}}/.git/hooks')
 os.symlink(src, dst)
