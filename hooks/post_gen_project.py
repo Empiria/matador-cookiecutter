8f6497d214635fbb095b49180f4000279e413bef
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:01:40 +0100

    Use subprocess.call

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,8 +7,7 @@ hooks_dir = os.path.join(project_dir, '.git/hooks')
 src = os.path.join(project_dir, 'src/utils/prepare-commit-msg.py')
 dst = os.path.join(hooks_dir, 'prepare-commit-msg')
 
-process = subprocess.Popen('git', 'init', project_dir)
-process.wait()
+process = subprocess.call('git', 'init', project_dir)
 
 os.mkdir('{{cookiecutter.repo_name}}/.git/hooks')
 os.symlink(src, dst)
