785b1a29478c9e9f962111dc5753a0a44bdc67f8
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-28 15:25:53 +0100

    Make git hook script executable

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 
-
-src = os.path.join(os.getcwd(),'src', 'utils', 'prepare-commit-msg.py')
+src = os.path.join(os.getcwd(), 'src', 'utils', 'prepare-commit-msg.py')
 dst = os.path.join('.git', 'hooks', 'prepare-commit-msg')
 
 process = subprocess.call(['git', 'init'])
