6232151c1a6594e477e9ca0138b50f8f78c698bb
Owen Campbell <owen.campbell@empiria.co.uk>
2017-06-21 17:06:32 +0100

    Remove example ticket and package

--- {{cookiecutter.project_name}}/deploy/packages/example-package/remove.py
+++
@@ -1,1 +0,0 @@
-from matador.commands.deployment import *
--- {{cookiecutter.project_name}}/deploy/tickets/example-ticket/deploy.py
+++
@@ -1,5 +0,0 @@
-from matador.commands.deployment import *
-
-ref = 't128'  # a valid git branch, tag or commit id
-
-DeploySqlScript('<path to sql script>', ref)
